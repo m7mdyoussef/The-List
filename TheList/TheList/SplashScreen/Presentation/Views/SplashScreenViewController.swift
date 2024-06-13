e253dddc7d358199d432141905c4fb0f9acb9630
mohamed youssef <mohamedyoussef@paymob.com>
2024-06-13 14:49:57 +0300

    add splash screen with home navigation

--- TheList/TheList/SplashScreen/Presentation/Views/SplashScreenViewController.swift
+++ TheList/TheList/SplashScreen/Presentation/Views/SplashScreenViewController.swift
@@ -2,13 +2,10 @@
 import UIKit
 
 class SplashScreenViewController: UIViewController {
-    @IBOutlet private weak var welcomeLabel: UILabel!
-    @IBOutlet private weak var movieLabel: UILabel!
     
     var viewModel: SplashViewModelContract!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
+        viewModel.navigateTo(to: .Home)
     }
 }
