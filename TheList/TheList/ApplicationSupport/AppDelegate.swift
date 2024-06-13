d8b49509d483755c3aba5a2bbc05069be08a5fcb
mohamed youssef <mohamedyoussef@paymob.com>
2024-06-13 13:00:09 +0300

    Build project Arch

---
+++ TheList/TheList/ApplicationSupport/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  TheList
+//
+//  Created by mohamed youssef on 13/06/2024.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+
+}
+
