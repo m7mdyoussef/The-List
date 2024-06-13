4072bae0916843cfec5f1316aed6860bf44e4ecd
mohamed youssef <mohamedyoussef@paymob.com>
2024-06-13 13:45:34 +0300

    add Network Layer

---
+++ TheList/TheList/Core/RemoteService/ApplicationNetworking.swift
@@ -0,0 +1,53 @@
+//
+//  ApplicationNetworking.swift
+//  TheList
+//
+//  Created by mohamed youssef on 13/06/2024.
+//
+
+import Foundation
+
+enum ApplicationNetworking{
+    case getList(page:String)
+}
+
+extension ApplicationNetworking : TargetType{
+    var baseURL: String {
+        switch self{
+        default:
+            return Constants.APIConstatnts.baseURL
+        }
+    }
+    
+    var path: String {
+        switch self{
+        case .getList:
+            return Constants.APIConstatnts.listUrlPath
+        }
+    }
+    
+    var method: HTTPMethod {
+        switch self{
+        default:
+            return .get
+        }
+    }
+    
+    var task: Task {
+        switch self{
+        case .getList(let page):
+            return .requestParameters(parameters:
+                                        [Constants.APIConstatnts.page:page],
+                                      encoding: .urlEncoding)
+        }
+    }
+    var headers: [String : String]? {
+        switch self{
+        default:
+            return [
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+            ]
+        }
+    }
+}
---
+++ TheList/TheList/Core/RemoteService/TargetType.swift
@@ -0,0 +1,52 @@
+//
+//  TargetType.swift
+//  TheList
+//
+//  Created by mohamed youssef on 13/06/2024.
+//
+
+import Foundation
+
+enum HTTPMethod : String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+}
+
+enum Task { //wrapper for my reqeust parameter
+    case requestPlain
+    case requestParameters(parameters:[String:Any],encoding: ParameterEncoding )
+}
+
+protocol TargetType { //wrapper that carries request properties
+    var baseURL : String {get}
+    var path: String {get}
+    var method:HTTPMethod {get}
+    var task:Task {get}
+    var headers: [String:String]? {get}
+}
+
+enum ParameterEncoding {
+    case urlEncoding
+    case jsonEncoding
+}
+
+
+extension ParameterEncoding {
+    func encode(urlRequest: inout URLRequest, parameters: [String: Any]?) throws {
+        switch self {
+        case .urlEncoding:
+            guard let parameters = parameters else { return }
+            var components = URLComponents(string: urlRequest.url!.absoluteString)!
+            components.queryItems = parameters.map { key, value in
+                URLQueryItem(name: key, value: "\(value)")
+            }
+            urlRequest.url = components.url
+        case .jsonEncoding:
+            guard let parameters = parameters else { return }
+            urlRequest.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: [])
+            urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        }
+    }
+}
